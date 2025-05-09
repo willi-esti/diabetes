c0e21602652c276a71aedc0e64ad8233f30ba205
it-techs <it.techs.fr@gmail.com>
2025-05-09 15:46:29 +0200

    dev: Add Prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier/flat');
 
 module.exports = defineConfig([
   expoConfig,
+  prettierConfig,
   {
     ignores: ['dist/*'],
   },
